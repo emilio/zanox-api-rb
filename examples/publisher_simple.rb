f44360dd7b767225a169db9aebbffa874377df75
ecoal95 <ecoal95@gmail.com>
2015-08-12 16:54:46 +0200

    Fix interactions between authenticated and unauthenticated methods

--- examples/publisher_simple.rb
+++ examples/publisher_simple.rb
@@ -2,4 +2,4 @@ require File.join(File.dirname(__FILE__),"..","lib","zanox_api.rb")
 
 client = Zanox::API::PublisherClient.new({ connect_id: 'XXX', secret_key: 'xxx' })
 
-puts client.get_sales.inspect
+puts client.authenticated_get_sales(date: Date.today).inspect
